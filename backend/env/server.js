4bff591fbae627bca3cd9e511b0e629d99132d7b
J Ben Anderson <revovisions@gmail.com>
2022-05-04 14:57:37 -0400

    more deployment troubleshooting

--- backend/env/server.js
+++ backend/env/server.js
@@ -1,7 +1,3 @@
 module.exports = ({ env }) => ({
-  proxy: true,
-  url: env("https://nextjs-strapi-blog.herokuapp.com"),
-  app: {
-    keys: env.array("APP_KEYS"),
-  },
+  url: env("HEROKU_URL"),
 });
